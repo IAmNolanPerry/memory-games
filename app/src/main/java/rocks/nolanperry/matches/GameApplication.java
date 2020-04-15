b91fc56d09ea9816eb03fcfec2e90ee8906d6cc0
Nolan Perry <github@nolanperry.rocks>
2020-04-14 20:29:39 -0400

    Add Instabug on the GameApplication.java File
    Initialize Instabug in the onCreate() method of the Application subclass

--- app/src/main/java/rocks/nolanperry/matches/GameApplication.java
+++ app/src/main/java/rocks/nolanperry/matches/GameApplication.java
@@ -10,6 +10,11 @@ public class GameApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		FontLoader.loadFonts(this);
+		new Instabug.Builder(this, "3c725f89e94d2eebe860a4ddc3025cb0")
+      .setInvocationEvents(
+        InstabugInvocationEvent.SHAKE,
+        InstabugInvocationEvent.FLOATING_BUTTON)
+      .build();
 
 	}
 }
